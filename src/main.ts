7713ddedc075caaf3f6f516c592dcfa6592cddb2
wayuto <4e07910@gmail.com>
2025-11-03 23:43:52 +0800

    fix: bug fix

--- src/main.ts
+++ src/main.ts
@@ -133,6 +133,18 @@ app.use(async (ctx, next) => {
     );
 });
 
+app.use(async (ctx, next) => {
+    ctx.response.headers.set("Access-Control-Allow-Origin", "*");
+    ctx.response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    ctx.response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+
+    if (ctx.request.method === "OPTIONS") {
+        ctx.response.status = 200;
+        return;
+    }
+
+    await next();
+});
 
 app.use(router.routes());
 app.use(router.allowedMethods());
